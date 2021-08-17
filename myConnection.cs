bdb8235507975b13d3366ca700b25fc1ea4bf442
Mariana Licona <m.licona@abtis.de>
2021-08-17 16:15:27 +0200

    add database name

--- myConnection.cs
+++ myConnection.cs
@@ -9,7 +9,7 @@ namespace U2A1IDEXXXx
          public static MySqlConnection GetConnection() // Metodo para hacer conexion
        {
 
-           MySqlConnection connector = new MySqlConnection("server=127.0.0.1; database=Tutorial; Uid=root; pwd=;");  //String de conexion
+           MySqlConnection connector = new MySqlConnection("server=127.0.0.1; database=dbu3mala; Uid=root; pwd=;");  //String de conexion
                connector.Open();
                return connector;        
        }
