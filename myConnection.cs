26e3ea07935412ea3fa3bd17852bb66d923f5388
Mariana Licona <m.licona@abtis.de>
2021-08-17 16:07:04 +0200

    fixed bug in GetConnection method

--- myConnection.cs
+++ myConnection.cs
@@ -1,22 +1,17 @@
 ﻿using MySql.Data.MySqlClient;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace U2A1IDEXXXx
 {
     public class MyConnection
     {
-        public static MySqlConnection ObtenerConexion()
-        {
+         public static MySqlConnection GetConnection() // Metodo para hacer conexion
+       {
 
-          MySqlConnection conectar = new MySqlConnection (“server = 127.0.0.1; database = Tutorial; Uid = root; pwd =;”);
-
-            conectar.Open();
-
-            return conectar;
-        }
+           MySqlConnection connector = new MySqlConnection("server=127.0.0.1; database=Tutorial; Uid=root; pwd=;");  //String de conexion
+               connector.Open();
+               return connector;        
+       }
     }
 }
