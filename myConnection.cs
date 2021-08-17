8d813c4b32f4aed51ed604ba0fa62e055a83b8c8
Mariana Licona <m.licona@abtis.de>
2021-08-17 06:52:49 +0200

    !!

--- myConnection.cs
+++ myConnection.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,16 @@ using System.Threading.Tasks;
 
 namespace U2A1IDEXXXx
 {
-    class myConnection
+    public class MyConnection
     {
+        public static MySqlConnection ObtenerConexion()
+        {
+
+          MySqlConnection conectar = new MySqlConnection (“server = 127.0.0.1; database = Tutorial; Uid = root; pwd =;”);
+
+            conectar.Open();
+
+            return conectar;
+        }
     }
 }
