d39c225d43d28e90c30a3d4464b38452865de311
KaiserinDerWelt <desjmarianaguilar@gmail.com>
2021-08-24 23:13:06 +0200

    retrieve db to combobox

--- Form1.cs
+++ Form1.cs
@@ -70,11 +70,17 @@ namespace U2A1IDEXXXx
 
         private void Form1_Load(object sender, EventArgs e) //Metodo form load
         {
-           
 
+            MySqlCommand myCommand = new MySqlCommand("SELECT idTipoEdoCivil FROM estadocivil;", connector); //string de conexion
+            connector.Open(); //Abrir mi conexion
+            MySqlDataReader myRegister = myCommand.ExecuteReader(); //Data reader
+            while (myRegister.Read()) //Mientras tenga registros que leer
+            {
+                CbxEdoCivil.Items.Add(myRegister["idTipoEdoCivil"].ToString()); //Pasar idTipoEdoCivil a string en combobox
+            }
+            connector.Close(); //Cierro mi conexion
     }
 
-
-
-}
+        
+    }
 }
