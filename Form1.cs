71b0f9bdb7386226b8dfd192a7c5f29f6cf1300e
KaiserinDerWelt <desjmarianaguilar@gmail.com>
2021-08-25 20:23:12 +0200

    add finally

--- Form1.cs
+++ Form1.cs
@@ -37,10 +37,10 @@ namespace U2A1IDEXXXx
 
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
+            
+    }
 
-        }
-
-        private void radioButtonMas_CheckedChanged(object sender, EventArgs e)
+    private void radioButtonMas_CheckedChanged(object sender, EventArgs e)
         {
 
         }
@@ -59,44 +59,43 @@ namespace U2A1IDEXXXx
 
       private void Form1_Load(object sender, EventArgs e) //Metodo form load
         {
-
            
-    }
+
+        }
 
         private void button3_Click(object sender, EventArgs e)
         {
             try
             {
-                string Query = "SELECT * FROM dbu3mala.estadocivil;";
-              //string QueryDrs = "SELECT * FROM dbu3mala.tbmedicos";
+                string Query = "SELECT * FROM dbu3mala.estadocivil;"; ;
+                // string QueryDrs = "SELECT * FROM dbu3mala.tbmedicos;";
                 connector.Open(); //Abrir mi conexion
-                MySqlCommand myCommand = new MySqlCommand(Query,  connector); ;
-             //MySqlCommand myCommandDrs = new MySqlCommand(QueryDrs, connector) ; 
+                MySqlCommand myCommand = new MySqlCommand(Query, connector);
+                // MySqlCommand myCommandDrs = new MySqlCommand(QueryDrs, connector); 
                 MySqlDataReader reader = myCommand.ExecuteReader(); //Data reader
-                //MySqlDataReader readerDrs = myCommandDrs.ExecuteReader();
+                                                                    // MySqlDataReader readerDrs = myCommandDrs.ExecuteReader();
                 while (reader.Read()) //Mientras tenga registros que leer
                 {
                     CbxEdoCivil.Items.Add(reader.GetString("idTipoEdoCivil"));
-                  //cbxMedicos.Items.Add(readerDrs.GetString("NombreCompleto"));
+                    //cbxMedicos.Items.Add(readerDrs.GetString("NombreCompleto"));
                 }
-            }
-            catch (Exception )
-            {
-                MessageBox.Show("Algo anda MUY mal!");
-            }
-
-            try
-            {
-
             }
             catch (Exception)
             {
-
-                throw;
+                MessageBox.Show("Algo anda MUY mal!");
             }
+            finally
+            {
+                connector.Close();
+            }
+
+
         }
+
+       
     }
 }
+
 
 
 
